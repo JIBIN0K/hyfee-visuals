c0400d4d3f9f5a544e01dd5ca65be5594f04093a
Jibin K <jibinkcalicut@gmail.com>
2026-07-03 12:01:00 +0530

    Add <Prompt Name>

--- content/prompts/index.ts
+++ content/prompts/index.ts
@@ -1,9 +1,11 @@
 import fluidPixelStretch from "./fluid-pixel-stretch";
 import mistyPrompt from "./misty-prompt";
+import goldenSunset from "./golden-sunset";
 
 export const prompts = [
-  fluidPixelStretch,
-  mistyPrompt,
+    fluidPixelStretch,
+    mistyPrompt,
+    goldenSunset,
 ];
 
 export function getPromptBySlug(slug: string) {
