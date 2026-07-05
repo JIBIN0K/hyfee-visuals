9d57053014eafa73bacd385d77a8382fb928219e
Jibin K <jibinkcalicut@gmail.com>
2026-07-05 19:45:33 +0530

    Add Lumina Prompt

--- content/prompts/index.ts
+++ content/prompts/index.ts
@@ -1,11 +1,13 @@
 import fluidPixelStretch from "./fluid-pixel-stretch";
 import mistyPrompt from "./misty-prompt";
 import goldenSunset from "./golden-sunset";
+import luminaPrompt from "./lumina-prompt";
 
 export const prompts = [
     fluidPixelStretch,
     mistyPrompt,
     goldenSunset,
+    luminaPrompt,
 ];
 
 export function getPromptBySlug(slug: string) {
